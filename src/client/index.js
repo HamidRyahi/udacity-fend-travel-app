f1a8310b69b9355e301c1b60613578ceba37843c
Hamid Ryahi <43662608+HamidRyahi@users.noreply.github.com>
2022-03-17 03:09:29 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,14 @@
-import { handleSubmit } from './js/app'
-import { mkmp } from './js/issMap'
-import { endInputHandler } from './js/endInputHandler'
-import {countdown} from './js/handleDays'
-import {lengthOfTrip} from './js/handleDays'
-import {todayDate} from './js/returnToday'
-import {buildTableFunc} from './js/buildTable'
-import './styles/style.scss'
-import './styles/weather-table.scss'
+import { handleSubmit } from './js/app';
+import { mkmp } from './js/issMap';
+import { endInputHandler } from './js/endInputHandler';
+import { countdown } from './js/handleDays';
+import { lengthOfTrip } from './js/handleDays';
+import { todayDate } from './js/returnToday';
+import { buildTableFunc } from './js/buildTable';
+import { dayORdays } from './js/dayORdays';
+import { updateImg } from './js/updateImg';
+import './styles/style.scss';
+import './styles/weather-table.scss';
 
 export {
     handleSubmit,
@@ -15,5 +17,7 @@ export {
     todayDate,
     endInputHandler,
     mkmp,
-    buildTableFunc
+    buildTableFunc,
+    dayORdays,
+    updateImg,
 }
